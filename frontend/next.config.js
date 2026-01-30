5b286d2092cb5c0cb8094ed763dc3ec700e39fad
durga <durgaprashadreddy9966@gmail.com>
2026-01-30 09:40:40 +0530

    Fix: Remove standalone output for Amplify hosting

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,8 +2,6 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   output: "standalone",
-   outputFileTracingRoot: path.join(__dirname),
    images: {
       domains: ['localhost'],
       remotePatterns: [
